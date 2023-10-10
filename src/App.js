b035189f4fc12fd3eac872da8387ba8986c9f29b
Javier Lete <javier@eskura21.com>
2023-10-10 19:35:18 +0200

    Versión con viaje en el tiempo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function Square({ value, onSquareClick }) {
   return <button onClick={onSquareClick} className="square">{value}</button>;
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null)); // useState([null, null, null, null, null, null, null, null, null])
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
 
   const winner = calculateWinner(squares);
 
@@ -26,8 +24,7 @@ export default function Board() {
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? 'X' : 'O';
 
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
+    onPlay(nextSquares);
   }
 
   return (
@@ -52,6 +49,51 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>
+          {moves}
+        </ol>
+      </div>
+    </div>
+  );
+}
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
